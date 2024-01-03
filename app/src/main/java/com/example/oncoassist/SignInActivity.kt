b7832a998c903f554dd199c2fe1405fa5d8dc2d1
Devanshupathak <101103249+Devanshupathak@users.noreply.github.com>
2024-01-03 13:32:08 +0530

    Homepage

--- app/src/main/java/com/example/oncoassist/SignInActivity.kt
+++ app/src/main/java/com/example/oncoassist/SignInActivity.kt
@@ -2,6 +2,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.oncoassist.ForgetPasswordActivity
+import com.example.oncoassist.MainActivity
+import com.example.oncoassist.SignUpActivity
 import com.example.oncoassist.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
 
@@ -29,8 +32,8 @@ class SignInActivity : AppCompatActivity() {
     }
 
     private fun userLogin() {
-        val email = binding?.semail?.text?.toString()?.trim() ?: ""
-        val password = binding?.spassword?.text?.toString()?.trim() ?: ""
+        val email = binding?.etSinInEmail?.text?.toString()?.trim() ?: ""
+        val password = binding?.etSinInPassword?.text?.toString()?.trim() ?: ""
 
         if (email.isEmpty() || password.isEmpty()) {
             Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
