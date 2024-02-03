9d02ab94dccb47d4763ca69593e399880b72f816
KHUSHI CHAUHAN <khushichauhan012345@gmail.com>
2024-02-04 00:17:15 +0530

    skin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,13 +48,14 @@ android {
 
     buildFeatures{
         viewBinding = true
+        mlModelBinding = true
     }
+
 }
 
 
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -65,11 +66,14 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
     implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("org.tensorflow:tensorflow-lite-support:0.1.0")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-database")
+    implementation("org.tensorflow:tensorflow-lite:1.14.0")
 }
 
